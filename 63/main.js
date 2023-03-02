8d504c367cc2d3e613249ac60131d442b6ce17c5
prem_prakash <prakashmadhepur@gmail.com>
2023-03-02 14:09:29 +0530

    initial commit

---
+++ 63/main.js
@@ -0,0 +1,50 @@
+var element;
+
+// element=document.getElementById("header").innerText;
+
+// element = document.getElementById("content").innerText;
+
+// element = document.getElementById("content").innerHTML;
+
+//element = document.getElementById("header").innerHTML;
+
+// element = document.getElementById("header").getAttribute("class");
+
+// element = document.getElementById("header").getAttribute("id");
+
+// element = document.getElementById("header").getAttribute("style");
+
+// element = document.getElementById("header").getAttribute("onClick");
+
+// element = document.getElementById("header").getAttributeNode("onClick");
+
+//element = document.getElementById("header").getAttributeNode("style");
+
+//element = document.getElementById("header").getAttributeNode("style").value;
+
+// element = document.getElementById("header").attributes;
+
+// element = document.getElementById("header").attributes[1];
+
+// element = document.getElementById("header").attributes[2].name;
+
+// element = document.getElementById("header").attributes[2].value;
+
+/* DOM Set Method */
+
+// document.getElementById("header").innerHTML = "<h1>WOW</h1>";
+
+// document.getElementById("header").innerText = "WOW";
+
+// document.getElementById("header").setAttribute("style","border:10px dotted yellow");
+
+// document.getElementById("header").setAttribute("class","xyz");
+// element = document.getElementById("header").getAttribute("class");
+
+//element = document.getElementById("header").attributes[1].value = "xyz";
+
+// element = document.getElementById("header").removeAttribute("style");
+
+// element = document.getElementById("header").removeAttribute("class");
+
+console.log(element);
